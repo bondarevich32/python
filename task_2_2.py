64cc05be4cfd0d5cc8219b1d52b2e9b902fa7eb9
bondarevich32 <122357860+bondarevich32@users.noreply.github.com>
2023-01-14 13:25:45 +0300

    Update task_2_2.py

--- task_2_2.py
+++ task_2_2.py
@@ -11,6 +11,6 @@ while i < element_count:
     i += 1
 
 for elem in range(int(len(my_list)/2)):
-        my_list[element], my_list[element + 1] = my_list [element + 1], my_list[element]
-        element += 2
+    my_list[element], my_list[element + 1] = my_list [element + 1], my_list[element]
+    element += 2
 print(my_list)
