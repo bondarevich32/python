19de224a01b8644a120588f972fda2a61c406331
bondarevich32 <122357860+bondarevich32@users.noreply.github.com>
2023-01-14 13:51:50 +0300

    Update task_1_2.py

--- task_1_2.py
+++ task_1_2.py
@@ -3,13 +3,6 @@
 # Использовать функцию type() для проверки типа.
 # Элементы списка можно не запрашивать у пользователя, а указать явно, в программе.
 
-my_list = [10, None, -30, 'True', True, 9.5]
-
-
-def my_type(element):
-    for element in range(len(my_list)):
-        print(type(my_list[element]))
-    return
-
-
-my_type(my_list)
+type_list = ['cat', 1, (2,), [1, 2], 3.1, False, frozenset('1'), set('1')]
+for el in type_list:
+    print(type(el))
